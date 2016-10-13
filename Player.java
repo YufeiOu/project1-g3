5108f1ae2f1876ac586919909d91d2281b0be2d9
Yufei Ou <yo2265@columbia.edu>
2016-10-12 23:38:58 -0400

    refactor building/road/helper,TODO: refactor and improve water and park

--- Player.java
+++ Player.java
@@ -24,118 +24,29 @@ public class Player implements pentos.sim.Player {
 	public Move play(Building request, Land land) {
 		// find all valid building locations and orientations
 		ArrayList<Move> moves = findBuildableMoves(request, land);
-
-		if (moves.isEmpty()) return new Move(false);
-		
-		// find all objective function values of each move, means "how good the move is"
+		// if there is not any buildable place, reject it
+		if (moves.isEmpty()) return new Move(false);	
+		// find all objective function values of each move, means "how bad the move is"
 		ArrayList<Integer> objs = findObjectiveOfMoves(moves, land, request);
 		// find the best 20 buildable moves in case some of them is detached from any raod
 		ArrayList<Integer> indexes = findSmallestObjs(objs, NUMBER_OF_RETRY);
 		// pick the best one among these candidates, this time include more time-consuming computation
 		Move chosen = findBestMove(moves, objs, indexes, land);
+		// record the place of chosen and its corresponding road
 		Set<Cell> shiftedCells = shiftedCellsFromMove(chosen);
 		Set<Cell> roadCells = findShortestRoad(shiftedCells, land);
-		
-
+		// check if there is no reachable place
+		if (roadCells == null) return new Move(false);
 		// start finding reasonable pond and field for resident
-		if (roadCells != null) {
-			chosen.road = roadCells;
-			road_cells.addAll(roadCells);
-			// for residences, build clever ponds and fields connected to it
-			if (request.type == Building.Type.RESIDENCE) { 
-
-				Set<Cell> markedForConstruction = new HashSet<Cell>();
-				markedForConstruction.addAll(roadCells);
-
-				// find smaller set strategy
-				Set<Cell> potentialWater = walkAndBuild(shiftedCells, markedForConstruction, land, 4, this.WATER, true);
-				Set<Cell> potentialPark = walkAndBuild(shiftedCells, markedForConstruction, land, 4, this.PARK, false);
-
-				if (potentialWater.size() == potentialPark.size()) {
-					// cannot build either
-					if (potentialWater.size() == 0) {
-						return chosen;
-					}
-					chosen.water = potentialWater;
-					markedForConstruction.addAll(chosen.water);
-					potentialPark = walkAndBuild(shiftedCells, markedForConstruction, land, 4, this.PARK, false);
-					chosen.park = potentialPark;
-					markedForConstruction.addAll(chosen.park);
-				}
-				else if (potentialWater.size() < potentialPark.size() && potentialWater.size() != 0) {
-					chosen.water = potentialWater;
-					markedForConstruction.addAll(chosen.water);
-					potentialPark = walkAndBuild(shiftedCells, markedForConstruction, land, 4, this.PARK, false);
-					chosen.park = potentialPark;
-					markedForConstruction.addAll(chosen.park);	
-				}
-				else {
-					chosen.park = potentialPark;
-					markedForConstruction.addAll(chosen.park);
-					potentialWater = walkAndBuild(shiftedCells, markedForConstruction, land, 4, this.WATER, true);
-					chosen.water = potentialWater;
-					markedForConstruction.addAll(chosen.water);
-				}
-			}
-			return chosen;
-		} else {
-			return new Move(false);
-		}
+		chosen.road = roadCells;
+		road_cells.addAll(roadCells);
+		if (request.type == Building.Type.RESIDENCE)
+			setWaterAndParkToResidence(chosen, shiftedCells, roadCells, land);
+		return chosen;
 	}
-
-	/* methods on buildings begin */
-	private ArrayList<Move> findBuildableMoves(Building request, Land land) {
-		ArrayList<Move> moves = new ArrayList<Move> ();
-		for (int i = 0 ; i < land.side ; i++) {
-			for (int j = 0 ; j < land.side ; j++) {
-				Cell p = new Cell(i, j);
-				Building[] rotations = request.rotations();
-				for (int ri = 0 ; ri < rotations.length ; ri++) {
-					Building b = rotations[ri];
-					if (land.buildable(b, p)) {
-						moves.add(new Move(true, request, p, ri, new HashSet<Cell>(), new HashSet<Cell>(), new HashSet<Cell>()));
-					}
-				}
-			}
-		}
-		return moves;
-	}
-
-	private Set<Cell> shiftedCellsFromMove(Move move) {
-		Set<Cell> shiftedCells = new HashSet<Cell>();
-		for (Cell x : move.request.rotations()[move.rotation])
-			shiftedCells.add(new Cell(x.i+move.location.i,x.j+move.location.j));
-		return shiftedCells;
-	}
-
-	private int edgeIncrease(Set<Cell> shiftedCells, Land land) {
-		int edge_increase = 0; // the increase of new edge: the less, the better
-		for (Cell shiftedCell : shiftedCells) {
-			for (Cell adjCell : shiftedCell.neighbors()) {
-				if (!shiftedCells.contains(adjCell) && land.unoccupied(adjCell))
-					edge_increase += 1;
-				if (!shiftedCells.contains(adjCell) && !land.unoccupied(adjCell))
-					edge_increase -= 2;
-			}
-		}
-		return edge_increase;
-	}
-
-	private ArrayList<Integer> findObjectiveOfMoves(ArrayList<Move> moves, Land land, Building request) {
-		ArrayList<Integer> objs = new ArrayList<Integer> ();
-		int i = 0;
-		for (Move move : moves) {
-			Set<Cell> shiftedCells = shiftedCellsFromMove(move);
-			int obj = 0;
-			obj += (request.type == Building.Type.FACTORY ? i : -i);
-			obj += 100 * edgeIncrease(shiftedCells, land);
-			// TODO: remove hardcode
-			objs.add(obj);
-			i += 1;
-		}
-		return objs;
-	}
-
+	
+	/* helper functions begin */
+	// after calculating the objective functions, findSmallestObjs return the indexes of smallest n objs in order
 	private ArrayList<Integer> findSmallestObjs(ArrayList<Integer> objs, int n) {
 		int index = 0;
 		int j = 0;
@@ -164,71 +75,7 @@ public class Player implements pentos.sim.Player {
 		}
 		return objList;
 	}
-
-	private Move findBestMove(ArrayList<Move> moves, ArrayList<Integer> objs, ArrayList<Integer> indexes, Land land) {
-		Move chosen = new Move(false);
-		Set<Cell> shiftedCells = new HashSet<Cell>();
-		Set<Cell> roadCells = new HashSet<Cell>();
-		Set<Cell> marked = new HashSet<Cell>(); // always empty in this case
-		int minimalValue = Integer.MAX_VALUE;
-		int miniIndex = 0;
-
-		for (Integer index : indexes) {
-			int tmpObj = objs.get(index);
-			chosen = moves.get(index);
-			shiftedCells = shiftedCellsFromMove(chosen);
-			roadCells = findShortestRoad(shiftedCells, land);
-			if (roadCells == null) {
-				continue;
-			} else { // long road punishment
-				tmpObj += 20 * roadCells.size();
-				// TODO: remove hardcode here
-			}
-			tmpObj += 20 * detachedNearbySlots(shiftedCells, marked, land);
-			if (tmpObj < minimalValue) {
-				minimalValue = tmpObj;
-				miniIndex = index;
-			}
-		}
-		return moves.get(miniIndex);
-	}
-
-	private int detachedNearbySlots(Set<Cell> potential, Set<Cell> marked, Land land) {
-		int slots = 0;
-		Set<Cell> oldMarked = new HashSet<Cell>(marked);
-		Set<Cell> newMarked = new HashSet<Cell>(marked);
-		newMarked.addAll(potential);
-
-		for (Cell p : potential) {
-			for (Cell q : p.neighbors()) {
-				if (!land.unoccupied(q) || potential.contains(q) || marked.contains(q)) continue;
-				if (detachedFromRoad(q, newMarked, land) && !detachedFromRoad(q, oldMarked, land)) slots += 1;
-			}
-		}
-		return slots;
-	}
-
-	private boolean detachedFromRoad(Cell cell, Set<Cell> marked, Land land) {
-		Queue<Cell> queue = new LinkedList<Cell>();
-		Set<Cell> visited = new HashSet<Cell>();
-		queue.add(cell);
-		visited.add(cell);
-		while (!queue.isEmpty()) {
-			Cell p = queue.remove();
-			Set<Cell> cells = new HashSet<Cell>();
-			cells.add(p);
-			if (isReached(cells, land)) return true;
-			for (Cell q : p.neighbors()) {
-				if (!land.unoccupied(q) || visited.contains(q) || marked.contains(q)) continue;
-				visited.add(q);
-				queue.add(q);
-			}
-		}
-		return false;
-	}
-	/* methods on buildings end */
-
-	/* methods on road begin */
+	// check if any of the cell in a set is direct connected to the road
 	private boolean isReached(Set<Cell> cells, Land land) {
 		for (int z=0; z<land.side; z++) {
 			if (
@@ -247,7 +94,129 @@ public class Player implements pentos.sim.Player {
 		}
 		return false;
 	}
+	// check if a cell is detached from any road after some cells are marked (occupied)
+	private boolean detachedFromRoad(Cell cell, Set<Cell> marked, Land land) {
+		Queue<Cell> queue = new LinkedList<Cell>();
+		Set<Cell> visited = new HashSet<Cell>();
+		queue.add(cell);
+		visited.add(cell);
+		while (!queue.isEmpty()) {
+			Cell p = queue.remove();
+			Set<Cell> cells = new HashSet<Cell>();
+			cells.add(p);
+			if (isReached(cells, land)) return true;
+			for (Cell q : p.neighbors()) {
+				if (!land.unoccupied(q) || visited.contains(q) || marked.contains(q)) continue;
+				visited.add(q);
+				queue.add(q);
+			}
+		}
+		return false;
+	}	
+	/* helper functions end */
 
+	/* methods on buildings begin */
+	// enumarate all possible buildable moves (consider rotations)
+	private ArrayList<Move> findBuildableMoves(Building request, Land land) {
+		ArrayList<Move> moves = new ArrayList<Move> ();
+		for (int i = 0 ; i < land.side ; i++) {
+			for (int j = 0 ; j < land.side ; j++) {
+				Cell p = new Cell(i, j);
+				Building[] rotations = request.rotations();
+				for (int ri = 0 ; ri < rotations.length ; ri++) {
+					Building b = rotations[ri];
+					if (land.buildable(b, p)) {
+						moves.add(new Move(true, request, p, ri, new HashSet<Cell>(), new HashSet<Cell>(), new HashSet<Cell>()));
+					}
+				}
+			}
+		}
+		return moves;
+	}
+	// corresponding shiftedCells determined by the move
+	private Set<Cell> shiftedCellsFromMove(Move move) {
+		Set<Cell> shiftedCells = new HashSet<Cell>();
+		for (Cell x : move.request.rotations()[move.rotation])
+			shiftedCells.add(new Cell(x.i+move.location.i,x.j+move.location.j));
+		return shiftedCells;
+	}
+	// objective function element: punish when increase new edges (too much edges means you probably have holes)
+	private int edgeIncrease(Set<Cell> shiftedCells, Land land) {
+		int edge_increase = 0; // the increase of new edge: the less, the better
+		for (Cell shiftedCell : shiftedCells) {
+			for (Cell adjCell : shiftedCell.neighbors()) {
+				if (!shiftedCells.contains(adjCell) && land.unoccupied(adjCell))
+					edge_increase += 1;
+				if (!shiftedCells.contains(adjCell) && !land.unoccupied(adjCell))
+					edge_increase -= 2;
+			}
+		}
+		return edge_increase;
+	}
+	// objective function element: punish when increase detached slot (the slot that can never be used by other buildings)
+	private int detachedNearbySlots(Set<Cell> potential, Set<Cell> marked, Land land) {
+		int slots = 0;
+		Set<Cell> oldMarked = new HashSet<Cell>(marked);
+		Set<Cell> newMarked = new HashSet<Cell>(marked);
+		newMarked.addAll(potential);
+
+		for (Cell p : potential) {
+			for (Cell q : p.neighbors()) {
+				if (!land.unoccupied(q) || potential.contains(q) || marked.contains(q)) continue;
+				if (detachedFromRoad(q, newMarked, land) && !detachedFromRoad(q, oldMarked, land)) slots += 1;
+			}
+		}
+		return slots;
+	}
+	// objective function of buildings
+	private ArrayList<Integer> findObjectiveOfMoves(ArrayList<Move> moves, Land land, Building request) {
+		ArrayList<Integer> objs = new ArrayList<Integer> ();
+		int i = 0;
+		for (Move move : moves) {
+			Set<Cell> shiftedCells = shiftedCellsFromMove(move);
+			int obj = 0;
+			obj += (request.type == Building.Type.FACTORY ? i : -i); // seperate factory and resident from top to bottom
+			obj += 100 * edgeIncrease(shiftedCells, land); // punishment
+			// TODO: remove hardcode
+			objs.add(obj);
+			i += 1;
+		}
+		return objs;
+	}
+	// given a list of good indexes, we use more computing resources to find the best one among them
+	private Move findBestMove(ArrayList<Move> moves, ArrayList<Integer> objs, ArrayList<Integer> indexes, Land land) {
+		Move chosen = new Move(false);
+		Set<Cell> shiftedCells = new HashSet<Cell>();
+		Set<Cell> roadCells = new HashSet<Cell>();
+		Set<Cell> marked = new HashSet<Cell>(); // always empty in this case
+		int minimalValue = Integer.MAX_VALUE;
+		int miniIndex = 0;
+
+		for (Integer index : indexes) {
+			int tmpObj = objs.get(index);
+			chosen = moves.get(index);
+			shiftedCells = shiftedCellsFromMove(chosen);
+			roadCells = findShortestRoad(shiftedCells, land);
+			if (roadCells == null) {
+				continue;
+			} else { 
+				// long road punishment
+				tmpObj += 20 * roadCells.size();
+				// TODO: remove hardcode here
+			}
+			// cut the space punishment
+			tmpObj += 20 * detachedNearbySlots(shiftedCells, marked, land);
+			// TODO: remove hardcode here
+			if (tmpObj < minimalValue) {
+				minimalValue = tmpObj;
+				miniIndex = index;
+			}
+		}
+		return moves.get(miniIndex);
+	}
+	/* methods on buildings end */
+
+	/* methods on road begin */
 	// build shortest sequence of road cells to connect to a set of cells b
 	private Set<Cell> findShortestRoad(Set<Cell> b, Land land) {
 		Set<Cell> output = new HashSet<Cell>();
@@ -301,6 +270,39 @@ public class Player implements pentos.sim.Player {
     /* methods on road ends */
 
     /* methods on park or water begin */
+    private void setWaterAndParkToResidence(Move chosen, Set<Cell> shiftedCells, Set<Cell> roadCells, Land land) {
+		Set<Cell> markedForConstruction = new HashSet<Cell>();
+		markedForConstruction.addAll(roadCells);
+
+		// find smaller set strategy
+		Set<Cell> potentialWater = walkAndBuild(shiftedCells, markedForConstruction, land, 4, this.WATER, true);
+		Set<Cell> potentialPark = walkAndBuild(shiftedCells, markedForConstruction, land, 4, this.PARK, false);
+
+		if (potentialWater.size() == potentialPark.size()) {
+			// cannot build either
+			if (potentialWater.size() == 0) {
+				return ;
+			}
+			chosen.water = potentialWater;
+			markedForConstruction.addAll(chosen.water);
+			potentialPark = walkAndBuild(shiftedCells, markedForConstruction, land, 4, this.PARK, false);
+			chosen.park = potentialPark;
+			markedForConstruction.addAll(chosen.park);
+		} else if (potentialWater.size() < potentialPark.size() && potentialWater.size() != 0) {
+			chosen.water = potentialWater;
+			markedForConstruction.addAll(chosen.water);
+			potentialPark = walkAndBuild(shiftedCells, markedForConstruction, land, 4, this.PARK, false);
+			chosen.park = potentialPark;
+			markedForConstruction.addAll(chosen.park);	
+		} else {
+			chosen.park = potentialPark;
+			markedForConstruction.addAll(chosen.park);
+			potentialWater = walkAndBuild(shiftedCells, markedForConstruction, land, 4, this.WATER, true);
+			chosen.water = potentialWater;
+			markedForConstruction.addAll(chosen.water);
+		}	
+	}
+
 	private int findNearbyPondOrParkDistance(Set<Cell> b, Set<Cell> marked, Land land, boolean pond) {
 		// return 0,1,2,3 to indicate we can add 0,1,2,3 water to reach the pond or park, return -1 to indicate no nearby facility exist
 		Set<Cell> distance1 = new HashSet<Cell>();
@@ -622,7 +624,4 @@ public class Player implements pentos.sim.Player {
 		return output;
 	}
 	/* methods on park and water end */
-	
 }
-
-
